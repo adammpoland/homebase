fe8830600a9b0e9903541371911874660deb653d
Adam Poland <37116467+adammpoland@users.noreply.github.com>
2019-06-16 17:18:06 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -6,28 +6,38 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/mydb";
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const fs = require('fs');
 
 const app = express();
 
 //promise
 mongoose.Promise = global.Promise;
+const keys = require('./config/keys');
+
 //connet to mongoose
-mongoose.connect('mongodb://localhost/homebase2')
-    .then(() => console.log('MongoDb Connected, hopefully this works'))
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true
+})
+    .then(() => console.log('Mongo is connected'))
     .catch(err => console.log(err));
+
 
     //body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+
 //load ideamodel
 require('./models/ideas');
 const Idea = mongoose.model('ideas');
+//const SavedURLs = mongoose.model('savedURLs');
+
 
 
 //set storage engiene
 const storage = multer.diskStorage({
     destination: './public/uploads/',
+   
     filename: function (req, file, cb) {
         cb(null, file.originalname);
         // cb(null,file.originalname+ '-' + Date.now() + path.extname(file.originalname));
@@ -37,8 +47,7 @@ const storage = multer.diskStorage({
 
 //init upload
 const upload = multer({
-    storage: storage,
-
+    storage: storage
 }).single('myFile');
 
 
@@ -46,23 +55,42 @@ const upload = multer({
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
+app.use(express.static("./views"));
 
-app.get('/', (req, res) => res.render('index'));
 
+app.get('/', (req, res) => {
+
+    //DATABASE TO ARRAY
+    Idea.find({}, (err, ideas) => {
+        if (err) return console.log(err);
+
+        res.render('index', { ideas: ideas });
+    });
+});
 
 
 app.post('/upload', (req, res) => {
     upload(req, res, (err) => {
         if (err) {
+            console.log("err");
+
+            console.log(err);
+
             res.render('index', {
                 msg: err
+                
             });
+
         } else {
             if (req.file == undefined) {
+                console.log(req.file);
+
                 res.render('index', {
                     msg: 'Error no file selected'
                 });
             } else {
+                console.log("the fuck?");
+
                 const newUser={
                     title: req.file.filename,
                     //details: req.body.details
@@ -71,28 +99,63 @@ app.post('/upload', (req, res) => {
                 new Idea(newUser)
                     .save()
 
-                res.render('index', {
+                Idea.find({}, (err, ideas) => {
+                    if (err) return console.log(err);
+            
+                    res.render('index', { 
+                        ideas: ideas
+                        //file: `uploads/${req.file.filename}`
 
-                    msg: 'Upload Succesful!!!',
-                    file: `uploads/${req.file.filename}`
-
-
+                    });
                 });
+               
 
 
             }
         }
-    });
+    })
 });
 
 
 
 
 //for downloads
-app.get('/download', function (req, res) {
-    let file = __dirname + '/public/uploads/'+req.query['title'];
+app.post('/download', function (req, res) {
+    let file = __dirname + '/public/uploads/'+req.body.title;
     console.log(file);
     res.download(file); // Set disposition and send it.
+});
+
+app.get('/browserView', function (req, res) {
+    let file = '/public/uploads/'+req.query['title'];
+    console.log(file);
+
+    var stream = fs.createReadStream(file);
+    var filename = req.query['title']; 
+    // Be careful of special characters
+
+    filename = encodeURIComponent(filename);
+    // Ideally this should strip them
+
+    res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+
+    stream.pipe(res);
+
+  
+});
+
+app.get('/savedURLs', function (req, res) {
+    //DATABASE TO ARRAY
+   
+        res.render('savedURLs');
+   
+});
+
+app.get('/contacts', function (req, res) {
+    //DATABASE TO ARRAY
+   
+        res.render('contacts');
+   
 });
 
 const port = 5000;
